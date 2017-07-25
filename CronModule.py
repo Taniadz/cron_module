604b0303c50089b635cb97f8fe5b4dccfee7e4cf
Taniadz <tdziubenko@ukr.net>
2017-07-25 15:20:45 +0300

    first commit

--- CronModule.py
+++ CronModule.py
@@ -1,6 +1,5 @@
 
-class Cron():
-
+class CronModule():
     def __init__(self, user, executable, minute=0, hour=0):
         self.user = user
         self.user = user
@@ -11,6 +10,26 @@ class Cron():
         self.MON = "*"  # Month field
         self.DOW = "*"  # day of week
 
+    @staticmethod
+    def check_for_minute(minute):
+        if minute > 59:
+            raise ValueError("Interval in minutes must not exceed 60!")
+
+    @staticmethod
+    def check_for_hour(hour):
+        if hour > 24:
+            raise ValueError("Interval in hours must not exceed 24!")
+
+    @staticmethod
+    def check_for_dow(day_of_week):
+        if day_of_week > 7:
+            raise ValueError("Interval in week must not exceed 7!")
+
+    @staticmethod
+    def check_for_dom(day_of_month):
+        if day_of_month > 31:
+            raise ValueError("Interval in month must not exceed 31!")
+
     def add_raw(self, raw_string):
         crontab_string = "{raw_string} {user} {executable}".format(
             raw_string=raw_string,
@@ -20,40 +39,31 @@ class Cron():
         return crontab_string
 
     def hourly(self, minute=0):
+        self.check_for_minute(minute)
         self.minute = minute
-        if self.minute > 59:
-            raise ValueError("Interval in minutes must not exceed 60!")
         self.hour = "*"
 
     def daily(self, minute=0, hour=0):
+        self.check_for_minute(minute)
+        self.check_for_hour(hour)
         self.minute = minute
-        if self.minute > 59:
-            raise ValueError("Interval in minutes must not exceed 60!")
         self.hour = hour
-        if self.hour > 24:
-            raise ValueError("Interval in hours must not exceed 24!")
 
-    def weekly(self, minute=0, hour=0, day_of_week = 1):
+    def weekly(self, minute=0, hour=0, day_of_week=1):
+        self.check_for_minute(minute)
+        self.check_for_hour(hour)
+        self.check_for_dow(day_of_week)
         self.minute = minute
-        if self.minute > 59:
-            raise ValueError("Interval in minutes must not exceed 60!")
         self.hour = hour
-        if self.hour > 24:
-            raise ValueError("Interval in hours must not exceed 24!")
         self.DOW = day_of_week
-        if self.DOW > 7:
-            raise ValueError("Interval in week must not exceed 7!")
 
     def monthly(self, minute=0, hour=0, day_of_month=1):
+        self.check_for_minute(minute)
+        self.check_for_hour(hour)
+        self.check_for_dom(day_of_month)
         self.minute = minute
-        if self.minute > 59:
-            raise ValueError("Interval in minutes must not exceed 60!")
         self.hour = hour
-        if self.hour > 24:
-            raise ValueError("Interval in hours must not exceed 24!")
         self.DOM = day_of_month
-        if self.DOM > 31:
-            raise ValueError("Interval in month must not exceed 31!")
 
     def create_cron(self):
         crontab_string = "{minute} {hour} {day_of_month} {month} {day_of_week} {user} {executable}".format(
@@ -69,14 +79,9 @@ class Cron():
 
 
 if __name__ == "__main__":
-    new1 = Cron(user="tania", executable="executable.py")
-    new1.monthly(minute=30, hour=3, day_of_month=3)
-    new1.create_cron()
-
-
-    new2 = Cron(user="tania", executable="python writeDate.py")
+    new2 = CronModule(user="tania", executable="python writeDate.py")
     new2.add_raw("* * * * *")
 
-    new3 = Cron(user="tania", executable="executable.py arg1")
-    new3.hourly()
+    new3 = CronModule(user="tania", executable="executable.py arg1")
+    new3.monthly(day_of_month=24)
     new3.create_cron()
---
+++ __init__.py
@@ -0,0 +1,2 @@
+from CronModule import CronModule
+
